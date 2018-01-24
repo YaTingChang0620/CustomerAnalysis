ccc8fdae0d987548a19e44c9efc383b8762612a8
ytc <yatingchang620@gmail.com>
2018-01-24 17:31:19 -0500

    add transactions/returns/upt

--- eCommerce Analysis.R
+++ eCommerce Analysis.R
@@ -1,43 +1,66 @@
-setwd("C:/Users/Ya Ting Chang/Desktop/kaggle/ecommerce-data")
 library(tidyverse)
 ecdf_raw<-read_csv("data.csv")
 ecdf <- ecdf_raw
-#which field has NA
+
+## Handling missing value and outlier ##
+#Determine the possible explaination for NA 
 apply(ecdf,2,function(x){sum(is.na(x))})
-#determine the possible explaination for NA 
 idNa <- ecdf[which(is.na(ecdf$CustomerID)),]
 apply(idNa,2,function(x){length(unique(x))})
 
-#remove rows which customerID is NA
-ecdf<-ecdf[!is.na(ecdf$CustomerID),]
-
-## distribution of quantity & Price ## 
+#distribution of unit price and quantity:
+#1).qunatity<0 -> cancelled/returned orders OR discount
 quantile(ecdf$Quantity)
-# transation with negative quantity? => returned goods or discount
 ecdf[ecdf$Quantity<0,]
-
-## distribution of price ##
+#2).unit price -> define price larger than 145 as outlier 
 quantile(ecdf$UnitPrice)
-# define price larger than 145 as outlier
-# since they account for 0.05% only
 quantile(ecdf$UnitPrice,probs=seq(0.99,1,0.0005))
+
+# remove rows:
+# 1). customerID is NA
+# 2). unitprice equals to 0
+# 3). uniprice outlier ($145) 
+ecdf<-ecdf[!is.na(ecdf$CustomerID),]
 ecdf <- ecdf[ecdf$UnitPrice<146,]
-# remove price equal to zero
 ecdf <- ecdf[!ecdf$UnitPrice==0,]
 
-## Exploration(by customers)##
-# 1.count the return
-customer <- data.frame(CustomerID=unique(ecdf$CustomerID))
 
-cust.stat <- ecdf %>%
-  filter(str_detect(InvoiceNo,"^C")&Description != "Discount") %>%
+## Exploration(by customers)##
+cust.stat <- data.frame(CustomerID=unique(ecdf$CustomerID))
+joinbyID <- function(df,id){
+  cust.stat <- cust.stat %>%
+    left_join(df,by=id)
+  return(cust.stat)
+}
+
+
+# total transactions:
+trans <- ecdf %>%
+  filter(!str_detect(InvoiceNo,"C")) %>%
   group_by(CustomerID) %>%
-  summarise(return=n())
-cust.stat <- left_join(customer,cust.stat,by="CustomerID")
+  summarise(transactions=length(unique(InvoiceNo)))
+cust.stat <- joinbyID(trans,"CustomerID")
+
+# returns:
+# 1). some customers might not return ALL products they purchased
+# 2). for returns>transactions: return products purchased earlier than year 2010 (49rows)
+ret <- ecdf %>%
+  filter(str_detect(InvoiceNo,"C")&Description != "Discount") %>%
+  group_by(CustomerID) %>%
+  summarise(return=length(unique(InvoiceNo)))
+cust.stat <- joinbyID(ret,"CustomerID")
 cust.stat <- cust.stat %>%
   mutate(return=ifelse(is.na(return),0,return))
 
-# 2.average consumption
+# Units per transaction (median)
+upt_temp <- ecdf %>%
+  filter(!str_detect(InvoiceNo,"C")) %>%
+  group_by(CustomerID,InvoiceNo) %>%
+  summarise(ttl=sum(Quantity))
+upt <- upt_temp %>%
+  group_by(CustomerID) %>%
+  summarise(unitPT=median(ttl))
+cust.stat <- joinbyID(upt,"CustomerID")
 
 
 
